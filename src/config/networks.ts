b00f321fa1a4b3b5ab742709694ae52fd5578a18
Jonathan Smirnoff <jonathan.smirnoff@iovlabs.org>
2024-09-03 14:28:16 -0300

    fix: serveral fixes

--- src/config/networks.ts
+++ src/config/networks.ts
@@ -14,13 +14,13 @@ export const networkConfig = {
     "rootstock": {
         network: "mainnet",
         easContractAddress: "0x54C0726E9d2D57Bc37AD52c7E219A3229e0eE963",
-        schemaRegistryContractAddress: "0x679c62956cD2801AbAbF80e9D430f18859Eea2d5",
+        schemaRegistryContractAddress: "0xeF29675d82CC5967069d6d9C17F2719f67728F5B",
         graphqlUrl: "http://localhost:4000/graphql"
     },
     "rootstock-testnet": {
-        network: "mainnet",
+        network: "testnet",
         easContractAddress: "0xc300aeEaDd60999933468738c9F5D7e9C0671e1c",
-        schemaRegistryContractAddress: "0xA7b39296258348C78294F95B872b282326A97BDF",
+        schemaRegistryContractAddress: "0x679c62956cD2801AbAbF80e9D430f18859Eea2d5",
         graphqlUrl: "http://localhost:4000/graphql"
     }
 
